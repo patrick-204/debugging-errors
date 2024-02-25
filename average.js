12e472ee26efa29e2b9786016f2474658b5685d8
patrick-204 <p.ohara921@gmail.com>
2024-02-25 14:24:12 -0500

    fixed errors

--- average.js
+++ average.js
@@ -1,10 +1,11 @@
 function average(list) {
-    var sum = 0;
+  var sum = 0;
   
-    for (var num of list) {
-      sum ++ num;
-    }
+  for (var num of list) {
+    sum += num;
+  }
   
-    return sum / list.length;
+  return sum / list.length;
+};
   
 console.log(average([3, 5, 7]));
